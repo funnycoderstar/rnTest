a184e38ba599f69b48759eb88832200c57e16a64
王亚星 <funnycoderstar@gmail.com>
2017-05-05 20:50:06 +0800

    feat: 添加react-native-tab-navigator

--- index.ios.js
+++ index.ios.js
@@ -12,21 +12,37 @@ import {
   View
 } from 'react-native';
 import Head from './src/home/head.js';
-
+import TabNavigator from 'react-native-tab-navigator';
 
 export default class rnTest extends Component {
   render() {
     return (
       <View style={styles.page}>
         <Head />
+        <TabNavigator hidesTabTouch={true} tabBarStyle={styles.tab}>
+          
+        </TabNavigator>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({  
-    page: {  
-        backgroundColor: '#f5f5f5',
-    }});  
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+  },
+  tab: {
+    height: 52,
+    backgroundColor: '#333333',
+    alignItems: 'center',
+  },
+  tabIcon: {
+    width: 30,
+    height: 35,
+    resizeMode: 'stretch',
+    marginTop: 12.5
+  }
+});
 
 AppRegistry.registerComponent('rnTest', () => rnTest);
